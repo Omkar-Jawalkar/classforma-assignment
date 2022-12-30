4fa2aa9dd5e66e363a99b9aa6d2a98338e0ecd25
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2022-12-30 23:16:33 +0530

    pdf added

--- src/pages/DocumentPage/index.tsx
+++ src/pages/DocumentPage/index.tsx
@@ -1,24 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { data } from "../../data/data";
 import { useParams } from "react-router-dom";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const DocumentPage = () => {
+  const [coordinates, setCoordinates] = useState([]);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
   const { myid } = useParams();
   const pdfData = data.pdfData.find((item) => item.id === parseInt(myid!));
   console.log(pdfData);
   return (
     <div className="flex flex-row text-lg ">
-      <div className="flex w-full flex-col">
-        <div className="min-h-full">
-          <h1>labels</h1>
+      <div className="flex min-w-[600px] flex-col">
+        <div className="min-h-full mx-8 space-y-2">
+          <h1 className=" border-b-2">labels</h1>
           <div className="flex flex-row space-x-4 ">
             <button className="bg-orange-400 px-2 py-1">Title</button>
             <button className="bg-green-400 px-2 py-1">Author</button>
           </div>
         </div>
-        <div className="min-h-full">bottom left</div>
+        <div className="min-h-full  mx-8 ">Box Coordinates</div>
       </div>
-      <div className="w-full">Right</div>
+      <div className="w-full">
+        <Document file={pdfData?.link} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
     </div>
   );
 };
